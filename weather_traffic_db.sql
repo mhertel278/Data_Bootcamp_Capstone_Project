29036628315d669e0b4f9fbdc00d795980c1ed0e
Michael Hertel <mhertel278@gmail.com>
2021-05-29 13:54:50 -0500

    indented queries for ease of reading

--- weather_traffic_db.sql
+++ weather_traffic_db.sql
@@ -62,22 +62,22 @@ Creating clean tables from raw_vehicle_traffic table
 
 -- check for duplicate date_time records in raw_vehicle_traffic table
 WITH RowNumCTE AS(
-SELECT *,
-	ROW_NUMBER() OVER(
-	PARTITION BY temp
-					
-					, rain_1h
-					, snow_1h
-					, clouds_all
-					, date_time
-					, traffic_volume
-						 ) row_num
-FROM raw_vehicle_traffic
-)
-SELECT *
-FROM RowNumCTE
--- WHERE row_num > 1
-;
+	SELECT *,
+		ROW_NUMBER() OVER(
+		PARTITION BY temp
+						
+						, rain_1h
+						, snow_1h
+						, clouds_all
+						, date_time
+						, traffic_volume
+							) row_num
+	FROM raw_vehicle_traffic
+	)
+	SELECT *
+	FROM RowNumCTE
+	-- WHERE row_num > 1
+	;
 
 --confirm duplicate date times have same concrete values, just different descriptive columns
 SELECT *
@@ -141,45 +141,45 @@ Clean holidays to solve for some dates saying None when it is a holiday
 -- determine if all date_times that are holidays have the holiday named
 
 WITH holidayCTE as (
-SELECT holiday, date_time::date as date
-FROM raw_vehicle_traffic
-) 
--- JOIN the CTE to itself where dates are same but holiday is not
-SELECT a.holiday as a_holiday
-	, a.date as a_date
-	, b.holiday as b_holiday
-	, b.date as b_date
-FROM holidayCTE a
-JOIN holidayCTE b
-	on a.date = b.date
-	AND a.holiday != b.holiday--) holiday_match
-
-ORDER BY a_date
-;
-
-
--- select distinct holiday and date from above query where holiday is not 'None' 
--- to create table of dates that are holidays
-WITH holidayCTE as (
-SELECT holiday, date_time::date as date
-FROM raw_vehicle_traffic
-) 
-SELECT DISTINCT a_holiday as holiday
-	, a_date as date
-INTO holiday
-FROM
-	(SELECT a.holiday as a_holiday
+	SELECT holiday, date_time::date as date
+	FROM raw_vehicle_traffic
+	) 
+	-- JOIN the CTE to itself where dates are same but holiday is not
+	SELECT a.holiday as a_holiday
 		, a.date as a_date
 		, b.holiday as b_holiday
 		, b.date as b_date
 	FROM holidayCTE a
 	JOIN holidayCTE b
 		on a.date = b.date
-		AND a.holiday != b.holiday
-	WHERE a.holiday!= 'None'
-	ORDER BY a_date) holiday_match
+		AND a.holiday != b.holiday--) holiday_match
 
-;
+	ORDER BY a_date
+	;
+
+
+-- select distinct holiday and date from above query where holiday is not 'None' 
+-- to create table of dates that are holidays
+WITH holidayCTE as (
+	SELECT holiday, date_time::date as date
+	FROM raw_vehicle_traffic
+	) 
+	SELECT DISTINCT a_holiday as holiday
+		, a_date as date
+	INTO holiday
+	FROM
+		(SELECT a.holiday as a_holiday
+			, a.date as a_date
+			, b.holiday as b_holiday
+			, b.date as b_date
+		FROM holidayCTE a
+		JOIN holidayCTE b
+			on a.date = b.date
+			AND a.holiday != b.holiday
+		WHERE a.holiday!= 'None'
+		ORDER BY a_date) holiday_match
+
+	;
 
 
 /*
@@ -191,34 +191,34 @@ FROM
 
 
 WITH VehicleCTE AS(  -- CTE to aggregate vehicle set on date
-SELECT AVG(avg_temp_f_hourly) as avg_temp_f_daily
-	, SUM(avg_rain_in_mm_hourly) as total_rain_mm_daily
-	, SUM(avg_snow_in_mm_hourly) as total_snow_mm_daily
-	, AVG(avg_cloud_percent_hourly) as avg_cloud_percent_daily
-	, SUM(vehicle_volume) as total_vehicle_volume_daily
-	, date
-	, EXTRACT(MONTH FROM date) as month
-	, EXTRACT(DOW FROM date) as day_of_week
-FROM cleaner_vehicle_traffic
-GROUP BY date
-)
+	SELECT AVG(avg_temp_f_hourly) as avg_temp_f_daily
+		, SUM(avg_rain_in_mm_hourly) as total_rain_mm_daily
+		, SUM(avg_snow_in_mm_hourly) as total_snow_mm_daily
+		, AVG(avg_cloud_percent_hourly) as avg_cloud_percent_daily
+		, SUM(vehicle_volume) as total_vehicle_volume_daily
+		, date
+		, EXTRACT(MONTH FROM date) as month
+		, EXTRACT(DOW FROM date) as day_of_week
+	FROM cleaner_vehicle_traffic
+	GROUP BY date
+	)
 
-, BikeCTE AS (  -- CTE to aggregate bike and pedestrian volume totals for Ramsey county
-SELECT SUM(total) as total
-	, date_day
-FROM raw_bike_pedestrian_traffic
-WHERE county = 'Ramsey'
-GROUP BY date_day
-)
+	, BikeCTE AS (  -- CTE to aggregate bike and pedestrian volume totals for Ramsey county
+	SELECT SUM(total) as total
+		, date_day
+	FROM raw_bike_pedestrian_traffic
+	WHERE county = 'Ramsey'
+	GROUP BY date_day
+	)
 
 
-SELECT b.total as daily_non_vehicle_traffic  -- join the aggregated vehicle and bike sets on common date
-	, v.*
- 	, COALESCE(h.holiday, 'none') as holiday -- create label 'none' if not a holiday
-INTO machine_learning_set
-FROM BikeCTE b
-INNER JOIN VehicleCTE v
-		on b.date_day = v.date
-LEFT JOIN holiday h
-	ON v.date = h.date
-;
+	SELECT b.total as daily_non_vehicle_traffic  -- join the aggregated vehicle and bike sets on common date
+		, v.*
+		, COALESCE(h.holiday, 'none') as holiday -- create label 'none' if not a holiday
+	INTO machine_learning_set
+	FROM BikeCTE b
+	INNER JOIN VehicleCTE v
+			on b.date_day = v.date
+	LEFT JOIN holiday h
+		ON v.date = h.date
+	;
